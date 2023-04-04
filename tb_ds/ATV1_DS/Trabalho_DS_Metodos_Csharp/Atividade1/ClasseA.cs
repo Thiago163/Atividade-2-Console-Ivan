9ba9c477fd45dc65a36b9591d2313c32cd7aeeae
Thiago_Cruz <101190863+Thiago163@users.noreply.github.com>
2023-04-04 13:18:05 -0300

    second commit

---
+++ tb_ds/ATV1_DS/Trabalho_DS_Metodos_Csharp/Atividade1/ClasseA.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalho_DS_Metodos_Csharp.Atividade1
+{
+    internal class ClasseA
+    {
+        static void Main(string[] args)
+        {
+            // Atividade 1
+            string categoria;
+
+            ClasseB cb = new ClasseB();
+
+            Console.Write("Digite o nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Digite o idade: ");
+            int idade_pessoa = Int32.Parse(Console.ReadLine());
+
+            if (idade_pessoa < 5)
+            {
+                Console.WriteLine("Desculpe, mas você é muito jovem");
+            }
+
+            else if (idade_pessoa < 8)
+            {
+                categoria = "Infantil A";
+
+                cb.verificar_idade( nome, idade_pessoa);
+                Console.WriteLine("Sua categoria é " + categoria);
+            }
+
+            else if (idade_pessoa < 11)
+            {
+                categoria = "Infantil B";
+
+                cb.verificar_idade( nome, idade_pessoa);
+                Console.WriteLine("Sua categoria é " + categoria);
+            }
+
+            else if (idade_pessoa < 14)
+            {
+                categoria = "Juvenil A";
+
+                cb.verificar_idade( nome, idade_pessoa);
+                Console.WriteLine("Sua categoria é " + categoria);
+            }
+
+            else if (idade_pessoa < 18)
+            {
+                categoria = "Juvenil B";
+
+                cb.verificar_idade( nome, idade_pessoa);
+                Console.WriteLine("Sua categoria é " + categoria);
+            }
+
+            else
+            {
+                categoria = "Adulto B";
+
+                cb.verificar_idade( nome, idade_pessoa);
+                Console.WriteLine("Sua categoria é " + categoria);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ tb_ds/ATV1_DS/Trabalho_DS_Metodos_Csharp/Atividade1/ClasseB.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalho_DS_Metodos_Csharp.Atividade1
+{
+    internal class ClasseB
+    {
+        // Atividade 1
+        public void verificar_idade(
+            string nome, int idade)
+        {
+            Console.WriteLine("Dados cadastrados\n\n");
+
+            Console.WriteLine(
+                "Nome: " + nome + "\n" +
+                "Idade: " + idade + "\n" 
+                );
+
+
+        }
+    }
+}
